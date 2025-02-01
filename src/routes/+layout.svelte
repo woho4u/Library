4eaad1d94f3570e5989eca13a9ece892f7a6e3b0
Oliver Eriksen <okebedrift@gmail.com>
2025-02-01 18:27:49 +0100

    Add Toast component for notifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-	import Header from '../components/Header.svelte'; 
+	import Header from '../components/Header.svelte';
+	import Toast from '../components/Toast.svelte';
+	import { showToast, toast } from '../stores/toast';
 	import '../app.css';
 </script>
 
 <Header />
-<div class="flex flex-col items-center h-screen px-14 py-8">
+<div class="flex h-screen flex-col items-center px-14 py-8">
 	<slot />
+</div>
+
+<div class="toast toast-end">
+	{#if $toast}
+		<Toast message={$toast.message} type={$toast.type} />
+	{/if}
 </div>
