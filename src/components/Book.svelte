60e27a25ba2069aa41f6b58aade5d5e79c1f07d7
Oliver Eriksen <okebedrift@gmail.com>
2025-02-01 18:28:49 +0100

    add hover effects, action buttons, keyword badges

--- src/components/Book.svelte
+++ src/components/Book.svelte
@@ -3,18 +3,52 @@
 </script>
 
 <div class="w-1/3 p-4">
-	<div class="card-sm card card-side h-52 w-full bg-base-100 p-4 shadow-xl">
+	<div class="card-sm border-1 border-gray50 card card-side h-52 w-full border p-4">
 		<figure>
 			<img src={book.coverImage} alt="Movie" class="h-full w-auto" />
 		</figure>
-		<div class="card-body">
-			<h2 class="card-title">{book.title}</h2>
-			<p>{book.description}</p>
-			<p>Written by {book.author}</p>
+		<div class="grow-1 card-body px-4 py-0">
+			<div>
+				<h2 class="card-title">{book.title}</h2>
+				<p class="text-secondary" style="">{book.author}</p>
+			</div>
+
+			<p>Genre: {book.genre}</p>
+			<div class="flex gap-2">
+				{#each book.keywords as keyword}
+					<span class="badge">{keyword}</span>
+				{/each}
+			</div>
+
 			<div class="card-actions justify-end"></div>
+			<div class="card-actions">
+				<button class="btn btn-primary btn-sm">Read</button>
+				<button class="btn btn-outline btn-primary btn-sm">Edit</button>
+			</div>
 		</div>
 	</div>
 </div>
 
 <style>
+	.card:hover {
+		transform: scale(1.05);
+		--tw-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);
+		--tw-shadow-colored: 0 20px 25px -5px var(--tw-shadow-color),
+			0 8px 10px -6px var(--tw-shadow-color);
+		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
+			var(--tw-shadow);
+	}
+	.card:hover .card-actions {
+		opacity: 1;
+		display: flex;
+	}
+	.card-actions {
+		opacity: 0;
+		transition: 200ms;
+		display: none;
+	}
+	.card {
+		transition: 200ms;
+		border-color: rgb(var(--));
+	}
 </style>
