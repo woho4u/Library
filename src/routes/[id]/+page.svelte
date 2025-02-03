29ab25a6b2f00ef06a620b23050adabce74f8acb
Oliver Eriksen <okebedrift@gmail.com>
2025-02-03 20:36:22 +0100

    add basic book details

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let data;
+	let book = data.book;
+</script>
+
+<div class="book-container flex flex-col items-center">
+	<h1>{book.title}</h1>
+	<h2>{book.author}</h2>
+	<p>{book.description}</p>
+	<p>{book.genre}</p>
+	<p>{book.keywords}</p>
+	<p>{book.published_date}</p>
+	<img src={book.coverImage} alt={book.title} />
+</div>
