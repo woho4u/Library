8143e0d71cf67420ee5b0002636c69817b89379c
Oliver Eriksen <okebedrift@gmail.com>
2025-02-03 22:01:22 +0100

    fix param fetch for book[id]

--- src/routes/[id]/+page.ts
+++ src/routes/[id]/+page.ts
@@ -6,23 +6,13 @@ export async function load({
 	params: any;
 }) {
 	const id = params.id;
-	console.log(id);
 
-	// const res = await fetch(`http://127.0.0.1:8000/api/books/?search=${id}`);
-	await fetch('http://127.0.0.1:8000/api/books/?search=' + id)
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`HTTP error! Status: ${response.status}`);
-			}
-			return response.json();
-		})
-		.then((data) => {
-			console.log(data);
-			return data;
-		})
-		.catch((error) => {
-			console.error('Error fetching books:', error);
-		});
+	const response = await fetch('http://127.0.0.1:8000/api/books/?search=' + id);
+	if (!response.ok) {
+		throw new Error(`HTTP error! Status: ${response.status}`);
+	}
 
-	// return book;
+	const data = await response.json();
+
+	return { book: data[0] };
 }
