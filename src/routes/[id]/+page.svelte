a29d5a7da208c9d0b3e29dd6f45b6b16eecd54ed
Oliver Eriksen <okebedrift@gmail.com>
2025-02-03 22:02:56 +0100

    Small fix in detail structure

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,14 +1,14 @@
-<script>
+<script lang="ts">
 	export let data;
 	let book = data.book;
 </script>
 
 <div class="book-container flex flex-col items-center">
+	<img src={book.coverImage} alt={book.title} />
 	<h1>{book.title}</h1>
 	<h2>{book.author}</h2>
 	<p>{book.description}</p>
 	<p>{book.genre}</p>
 	<p>{book.keywords}</p>
 	<p>{book.published_date}</p>
-	<img src={book.coverImage} alt={book.title} />
 </div>
