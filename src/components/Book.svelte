c58eb3e8dc13bf84dae84ab77b7b880e7d5b6617
Oliver Eriksen <okebedrift@gmail.com>
2025-02-04 21:46:14 +0100

    Add button for readingleast to book

--- src/components/Book.svelte
+++ src/components/Book.svelte
@@ -1,9 +1,20 @@
 <script>
+	import Icon from '@iconify/svelte';
+
 	let { book } = $props();
+	let user = localStorage.getItem('loginId');
+	console.log('User:', user);
+
+	function addToList() {
+		console.log('Add to list');
+	}
 </script>
 
 <div class="w-1/3 p-4">
 	<div class="card-sm border-1 border-gray50 card card-side h-52 w-full border p-4">
+		<button class="btn btn-ghost btn-sm absolute right-0 top-0" onclick={addToList}>
+			<Icon icon="material-symbols:format-list-bulleted-add" width="28" height="28" />
+		</button>
 		<figure>
 			<a href={'/' + book.id}>
 				<img src={book.coverImage} alt="Movie" class="h-full w-auto" />
