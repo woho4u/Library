c1732f7516bbf6d388c6efa6329dc1ecdef622e5
Oliver Eriksen <okebedrift@gmail.com>
2025-02-08 23:17:45 +0100

    Refactor load function signature in +page.ts for improved readability

--- src/routes/reading-lists/+page.ts
+++ src/routes/reading-lists/+page.ts
@@ -1,5 +1,7 @@
-export async function load({ fetch, params }: { fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>; params: any; }) {
-   const id = params.id;
-
-   const response = await fetch ('http://127.0.0.1:8000/api/books/?id_in=1,2')
-}
+export async function load({
+	fetch,
+	params
+}: {
+	fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+	params: any;
+}) {}
